473f6e769870c1af7a9e80eb8b60c969086f9b72
vbalalla <vibodha.balalla@syscolabs.com>
2018-02-12 17:40:54 +0530

    New: Test profile added.

--- src/test/java/com/sysco/ordermanager/domain/repository/OrderRepositoryTest.java
+++ src/test/java/com/sysco/ordermanager/domain/repository/OrderRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -13,6 +14,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles(profiles = {"test"})
 @WebAppConfiguration
 @SpringBootTest
 public class OrderRepositoryTest {

b214ebf53c65f474dc2f7bfcd1cac0bdbe7b6c17
vbalalla <vibodha.balalla@syscolabs.com>
2018-02-12 16:13:09 +0530

    custom query added to OrderRepository.

--- src/main/java/com/sysco/ordermanager/domain/repository/OrderRepository.java
+++ src/main/java/com/sysco/ordermanager/domain/repository/OrderRepository.java
@@ -4,9 +4,12 @@ import com.sysco.ordermanager.domain.model.OrderData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by vibodhab on 2/8/18.
  */
 @Repository
 public interface OrderRepository extends JpaRepository<OrderData, String> {
+    public List<OrderData> findByType(String type);
 }

18a9006976183955459cf36895775436ce5f4915
Heshan-Sandamal <mhsandamal1@gmail.com>
2018-02-16 18:03:25 +0530

    Wrote the tests for set order function

--- src/main/java/com/sysco/ordermanager/web/api/Order.java
+++ src/main/java/com/sysco/ordermanager/web/api/Order.java
@@ -3,7 +3,6 @@ package com.sysco.ordermanager.web.api;
 import com.sysco.ordermanager.util.enums.OrderStatus;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -12,45 +11,30 @@ import java.util.Set;
 public class Order {
     private Long id;
     private String type;
-    private User user;
+    private UserDTO userDTO;
     private OrderStatus orderStatus;
     private Set<OrderItem> orderItems;
 
     public Order() {
     }
 
-    public Order(Long id, String type, User user, OrderStatus orderStatus) {
+    public Order(Long id, String type, UserDTO userDTO, OrderStatus orderStatus) {
         this.id = id;
         this.type = type;
-        this.user = user;
+        this.userDTO = userDTO;
         this.orderStatus = orderStatus;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Order order = (Order) o;
-        return Objects.equals(getId(), order.getId());
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(getId());
-    }
-
-    public Order(String type, User user, OrderStatus orderStatus) {
+    public Order(String type, UserDTO userDTO, OrderStatus orderStatus) {
         this.type = type;
-        this.user = user;
-
+        this.userDTO = userDTO;
         this.orderStatus = orderStatus;
     }
 
-    public Order(Long id, String type, User user, OrderStatus orderStatus, Set<OrderItem> orderItems) {
+    public Order(Long id, String type, UserDTO userDTO, OrderStatus orderStatus, Set<OrderItem> orderItems) {
         this.id = id;
         this.type = type;
-        this.user = user;
+        this.userDTO = userDTO;
         this.orderStatus = orderStatus;
         this.orderItems = orderItems;
     }
@@ -63,12 +47,12 @@ public class Order {
         this.orderStatus = orderStatus;
     }
 
-    public User getUser() {
-        return user;
+    public UserDTO getUser() {
+        return userDTO;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setUser(UserDTO userDTO) {
+        this.userDTO = userDTO;
     }
 
     public Long getId() {
